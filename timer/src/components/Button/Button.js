c53720ecd7223ec6bc1ca1a03e83fd66b50884ad
Paweł <p.grabczak@cognitionhub.com>
2024-09-21 17:36:39 +0200

    prawie dobrze

--- timer/src/components/Button/Button.js
+++ timer/src/components/Button/Button.js
@@ -2,7 +2,7 @@ import style from './Button.module.scss';
 
 const Button = props => {
     return (
-        <button className={style.button}>{props.children}</button> // przez props.children możemy przekazać treść jak w tym wypadku buttona, alternatywa to props.text a w miejscu gdzie deklarujemy guzik text="nasz tekst"
+        <button className={style.button} onClick={props.onClick}>{props.children}</button> // przez props.children możemy przekazać treść jak w tym wypadku buttona, alternatywa to props.text a w miejscu gdzie deklarujemy guzik text="nasz tekst"
     );
 };
 
--- timer/src/components/Timer/Timer.js
+++ timer/src/components/Timer/Timer.js
@@ -1,15 +1,66 @@
 import Button from '../Button/Button';
 import style from './Timer.module.scss';
 
+import React, { useState, useEffect } from "react";
+
 const Timer = () => {
+
+    const [time, setTime] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+    const [intervalId, setIntervalId] = useState(null);
+  
+    useEffect(() => {
+      if (isRunning) {
+        const id = setInterval(() => {
+          setTime(prevTime => prevTime + 1);
+        }, 10);
+        setIntervalId(id);
+      }
+  
+      return () => {
+        if (intervalId) {
+          clearInterval(intervalId);
+        }
+      };
+    }, [isRunning]);
+  
+    const start = () => {
+      if (!isRunning) {
+        setIsRunning(true);
+      }
+    };
+  
+    const stop = () => {
+      if (isRunning) {
+        clearInterval(intervalId);
+        setIsRunning(false);
+      }
+    };
+  
+    const reset = () => {
+      clearInterval(intervalId);
+      setIsRunning(false);
+      setTime(0);
+    };
+  
+
+    const minutes = Math.floor((time % 360000) / 6000);
+    const seconds = Math.floor((time % 6000) / 100);
+    const milliseconds = time % 100;
+    const centiseconds = Math.floor(time % 10);
     return (
         <div className={style.mainDiv}>
             <h1>Timer</h1>
-            <p>Tu będzie świę wyświetlać czas</p>
+            <p>
+            {minutes.toString().padStart(2, "0")}:
+            {seconds.toString().padStart(2, "0")}:
+            {milliseconds.toString().padStart(2, "0")}.
+            {centiseconds}
+            </p>
             <div className={style.buttonDiv}>
-                <Button>Start</Button>
-                <Button>Stop</Button>
-                <Button>Reset</Button>
+                <Button onClick={start}>Start</Button>
+                <Button onClick={stop}>Stop</Button>
+                <Button onClick={reset}>Reset</Button>
             </div>
         </div>
     );
