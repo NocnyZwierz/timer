8df27d9f8c8d534b62486ca1bb5506fd2ecb0a46
Paweł <p.grabczak@cognitionhub.com>
2024-09-21 14:16:58 +0200

    :)

--- timer/src/App.js
+++ timer/src/App.js
@@ -3,7 +3,7 @@ import Container from "./components/Container/Container";
 function App() {
   return (
     <Container>
-      <h1>Timer App start</h1>
+      <h1>Timer App start!</h1>
     </Container>
   );
 }
